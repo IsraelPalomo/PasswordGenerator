e77d0490007bfec291bec5ca7f4db10aeaedd357
Israel <israpalomoe@gmail.com>
2024-06-23 17:46:03 +0200

    feature: add basic component and reactive

---
+++ src/components/Button.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="button" @click="$emit('btnAction')" >
+    Generate Password
+  </div>
+</template>
+
+<script setup>
+
+
+
+</script>
+
+<style lang="css" scoped>
+ .button {
+    width: fit-content;
+    padding: .5rem 2rem;
+    border-radius: 8px;
+    background-color: rgb(71, 31, 190);
+    color: #ffffff;
+    display: flex;
+    align-items: center;
+    align-items: center;
+    cursor: pointer;
+  }
+  .button:hover {
+    background-color: rgb(62, 28, 165);
+  }
+</style>
---
+++ src/components/Card.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="card">
+    <p>{{ password }}</p>
+    <Button @btnAction="generate"/>
+  </div>
+</template>
+
+<script setup>
+import Button from './Button.vue'
+import {ref} from 'vue'
+
+const password = ref('');
+
+function generate(){
+    console.log('...')
+    // Definir el conjunto de caracteres: letras (mayúsculas y minúsculas), números y símbolos
+    const caracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+-=[]{}|;:,.<>?';
+
+    // Inicializar la contraseña como una cadena vacía
+    let contraseña = '';
+
+    // Seleccionar 5 caracteres aleatorios del conjunto de caracteres
+    for (let i = 0; i < 5; i++) {
+        const indiceAleatorio = Math.floor(Math.random() * caracteres.length);
+        contraseña += caracteres[indiceAleatorio];
+    }
+
+    console.log(contraseña)
+
+    password.value =  contraseña;
+}
+
+</script>
+
+<style lang="css" scoped>
+.card{
+    padding: 1.5rem;
+    background-color: #323232;
+}
+</style>
